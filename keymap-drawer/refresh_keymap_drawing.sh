8c43a38550521afd7b29c739e4a3fc845d92413c
Flo Kempenich <flo@kempenich.dev>
2023-09-26 10:18:16 +0100

    Tweak refresh drawing script to watch for changes

---
+++ keymap-drawer/refresh_keymap_drawing.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR"
+
+CONFIG=./config.yaml
+KEYMAP=../config/corne.keymap
+
+refresh_drawing() {
+    echo "Refreshing drawing"
+    keymap -c $CONFIG \
+        parse \
+        -z $KEYMAP \
+        > ./corne.yaml
+
+    keymap -c $CONFIG \
+        draw \
+        ./corne.yaml \
+        > ./corne.svg
+    echo "Refresh done!"
+}
+
+refresh_drawing
+printf "\nWatching for changes on $CONFIG and $KEYMAP\n"
+fswatch -0 $CONFIG $KEYMAP | while read -d "" modified_file
+do
+    echo "$(basename $modified_file) was modified"
+    refresh_drawing
+    printf "\nWatching for changes on $CONFIG and $KEYMAP\n"
+done
+
+
+
+
+
+cd - >/dev/null
