62ed17ee6652bb9c3ee1dee694925a141ff22ebc
Flo Kempenich <flo@kempenich.dev>
2023-10-17 15:07:53 +0100

    Fix consistency issue with AK & Combo

--- config/flo_config.h
+++ config/flo_config.h
@@ -61,6 +61,7 @@
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right-hand keys
 #define THUMBS LH0 LH1 LH2 RH0 RH1 RH2                                      // thumb keys
+#define ALL_KEYS KEYS_L KEYS_R THUMBS                                       // all keys
 
 
 
@@ -174,7 +175,9 @@ Here, `_zmk_keymap_layer_state` is the 'uint32_t' bit array.
 //  - To make sure it's not too long  ⇒ ???
 //  - To make sure it's not too short ⇒ Try typing 'sl...'. 's' is on the pinkie so that's as slow as it gets
 
-
+// See explanation in flo_combos.dtsi
+#define ak_tap_time 10
+#define hold_key_event_delay 15 // ak_tap_time + 5ms buffer
 
 
 
