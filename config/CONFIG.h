37cbd46189826f01db4dacf23375801265562d17
Flo Kempenich <flo@kempenich.dev>
2023-11-25 11:56:25 +0000

    Remove global-quick-tap-ms from homerow_mods behaviors

--- config/CONFIG.h
+++ config/CONFIG.h
@@ -6,7 +6,6 @@
 #define my_ak_window 100 // I think it makes sense to have it equal to the linger term, it's the same sort of "feeling"
 #define my_combo_timeout_one_hand 18 // Tuned at 18ms, but sometimes missing the combo, so trying a bit higher. 20ms was too long, so trying again at 18ms
 #define my_combo_timeout_two_hands 30 // 40ms was good but sometimes it would trigger when I didn't want it to
-#define my_global_quick_tap_window 100 // TODO: Migrate to 'require-prior-idle-ms' | For now trying the same as linger term. Original: Measured w/ logs, 130ms is a comfortable quick double-tap for me
 #define sticky_key_release_timeout 1000
 #define sticky_key_release_timeout_long 3000
 // Calibration Tips
@@ -21,7 +20,6 @@
 // See explanation in flo_combos.dtsi
 #define ak_tap_time 10
 #define hold_key_event_delay 15 // ak_tap_time + 5ms buffer
-
 
 
 
