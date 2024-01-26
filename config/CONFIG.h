abaf44d20481ee6f3afb7a2a56fc7abf556de62a
Flo Kempenich <flo@kempenich.dev>
2024-01-26 12:18:28 +0000

    Timings: Tweak

--- config/CONFIG.h
+++ config/CONFIG.h
@@ -2,10 +2,10 @@
 /****                 Config                     ****/
 /****************************************************/
 #define linger_term 100 // Tuned ✅
-#define my_tapping_term 200 // Now trying the tapping term to be the same as the linger term. UPDATE: It was too short, so trying 120ms. UPDATE: 120ms was still too short, trying 200ms.
-#define require_prior_idle_ms 240 // When typing at 50wpm, there is on average 240ms between keys: 1/(50*5/60000)
+#define my_tapping_term 160 // Now trying the tapping term to be the same as the linger term. UPDATE: It was too short, so trying 120ms. UPDATE: 120ms was still too short, trying 200ms. 200 was too long
+#define require_prior_idle_ms 100 // When typing at 50wpm, there is on average 240ms between keys: 1/(50*5/60000). But that was too long, so I'm trying the same as the linger_term
 #define my_ak_window 100 // I think it makes sense to have it equal to the linger term, it's the same sort of "feeling"
-#define my_combo_timeout_adjacent 15
+#define my_combo_timeout_adjacent 17
 #define my_combo_timeout_non_adjc 20 // Tuned at 18ms, but sometimes missing the combo, so trying a bit higher. 20ms was too long, so trying again at 18ms
 #define my_combo_timeout_two_hnds 30 // 30ms was good but sometimes it would trigger when I didn't want it to
 #define sticky_key_release_timeout 1000
