f94a5c63760ccd0fa0f16cf5b76fde52b6836ca4
Flo Kempenich <flo@kempenich.dev>
2024-03-06 14:28:48 +0000

    Centralize preprocessor macros (& add 2)

--- config/features/__BASE__/preprocessor_macros.c
+++ config/features/__BASE__/preprocessor_macros.c
@@ -1,3 +1,27 @@
+#define STRINGIFY(ARG) #ARG
+
+
+
+#define REPLACE_CHAR_WITH_BIGRAM(BIGRAM_0, BIGRAM_1) \
+        replace_char_with_bigram_##BIGRAM_0##BIGRAM_1: replace_char_with_bigram##BIGRAM_0##BIGRAM_1 { \
+                label = STRINGIFY(flo-macro-replace-char-with-bigram-##BIGRAM_0##BIGRAM_1); \
+                compatible = "zmk,behavior-macro"; \
+                #binding-cells = <0>; \
+                wait-ms = <0>; \
+                tap-ms = <10>; \
+                bindings = \
+                        <&macro_tap &kp BACKSPACE>, \
+\
+                        <&macro_tap &kp BIGRAM_0>, \
+\
+                        <&macro_release &kp LSHFT>, \
+                        <&macro_release &kp RSHFT>, \
+\
+                        <&macro_tap ak_##BIGRAM_1>; \
+        };
+
+
+
 #define COMBO_LAY_BASE(LAYERS, NAME, BINDINGS, KEYPOS, TIMEOUT) \
   combo_##NAME { \
     bindings = <BINDINGS>; \
@@ -27,3 +51,24 @@
 // TODO: Rename to COMBO_LCK_TWO_HNDS
 #define COMBO_LCK_TWO_HAND(NAME, BINDINGS, KEYPOS) \
   COMBO_LAY_BASE(l_all_except_winpc_lock l_winpc l_lock, NAME, BINDINGS, KEYPOS, my_combo_timeout_two_hnds) // These combos work on any layers, EVEN the Lock layer!
+
+
+
+#define TYPING_MACRO(NAME, KP_BINDINGS) \
+ZMK_MACRO(NAME, \
+        wait-ms = <10>; \
+        tap-ms = <10>; \
+        bindings = <KP_BINDINGS>; \
+)
+
+
+
+#define LK(NAME, TAP_BINDING, LINGER_BINDING) \
+    lk_##NAME: lk_##NAME { \
+        compatible = "zmk,behavior-hold-tap"; \
+        label = "lk_##NAME"; \
+        #binding-cells = <2>; \
+        tapping-term-ms = <linger_term>; \
+        flavor = "tap-preferred"; \
+        bindings = <LINGER_BINDING>, <TAP_BINDING>; \
+    };
