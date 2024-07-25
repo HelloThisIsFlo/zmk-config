dd0015b996122e43562265de423f85351541d323
Flo Kempenich <flo@kempenich.dev>
2024-07-25 14:22:46 +0100

    Timings: Make 'thumb' timings the same as 'main' ones actually

--- config/CONFIG.h
+++ config/CONFIG.h
@@ -3,7 +3,7 @@
 /****************************************************/
 #define linger_term 100 // Tuned ✅
 #define my_tapping_term_main 200 // For the longest time, it was 130. But by separating hrm and thumb, I can afford to have the HRM longer, as I believe it's mostly the Layer that needs to be quick.
-#define my_tapping_term_thumb 100 // NOT TUNED YET, maybe same as linger_term would be good (to keep the same intuition)
+#define my_tapping_term_thumb 200 // NOT TUNED YET - UPDATE: Actually, maybe the same as the main one is OK actually, because we're using 'balanced' flavor anyway, maybe same as linger_term would be good (to keep the same intuition)
 #define require_prior_idle_ms 100 // NOT USED ANYMORE!! When typing at 50wpm, there is on average 240ms between keys: 1/(50*5/60000). But that was too long, so I'm trying the same as the linger_term
 #define my_ak_window 100 // I think it makes sense to have it equal to the linger term, it's the same sort of "feeling"
 #define my_combo_timeout_adjacent 17
