2d4641a1710e8c36e446bf768e3f75b3d91eb2c4
Flo Kempenich <flo@kempenich.dev>
2024-07-25 12:40:38 +0100

    Timings: Separate between Main & Thumb timings

--- config/CONFIG.h
+++ config/CONFIG.h
@@ -2,7 +2,8 @@
 /****                 Config                     ****/
 /****************************************************/
 #define linger_term 100 // Tuned ✅
-#define my_tapping_term 130 // Now trying the tapping term to be the same as the linger term. UPDATE: It was too short, so trying 120ms. UPDATE: 120ms was still too short, trying 200ms. 200 was too long
+#define my_tapping_term_main 200 // For the longest time, it was 130. But by separating hrm and thumb, I can afford to have the HRM longer, as I believe it's mostly the Layer that needs to be quick.
+#define my_tapping_term_thumb 100 // NOT TUNED YET, maybe same as linger_term would be good (to keep the same intuition)
 #define require_prior_idle_ms 100 // NOT USED ANYMORE!! When typing at 50wpm, there is on average 240ms between keys: 1/(50*5/60000). But that was too long, so I'm trying the same as the linger_term
 #define my_ak_window 100 // I think it makes sense to have it equal to the linger term, it's the same sort of "feeling"
 #define my_combo_timeout_adjacent 17
@@ -31,7 +32,7 @@
 /****************************************************/
 #define TEMP_macro_tap_time 10 // TODO: Actually there is a setting to configure that already. Use it.
 &mt {
-    tapping-term-ms = <my_tapping_term>;
+    tapping-term-ms = <my_tapping_term_main>;
 };
 
 
