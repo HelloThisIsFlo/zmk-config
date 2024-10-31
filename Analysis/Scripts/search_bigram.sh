490d673604865ba899fd367e6d09405f3031c6a6
Flo Kempenich <flo@kempenich.dev>
2024-10-31 14:42:36 +0000

    Naquadah: YB => BB (experiment)

---
+++ Analysis/Scripts/search_bigram.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR"
+
+bigram="$1"
+
+cat Corpus-Notion.txt | grep "\w*$bigram\w*" -o | sort | uniq | grep --color=always "$bigram"
+
+cd - >/dev/null
